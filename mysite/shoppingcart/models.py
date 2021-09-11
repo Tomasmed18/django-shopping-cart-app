cdf2ed2170e441269b6ba84453141dd110c2a75a
Tomasmed18 <tomy-medina@hotmail.com>
2021-09-11 19:35:06 -0300

    Create Product model

--- mysite/shoppingcart/models.py
+++ mysite/shoppingcart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    unit_price = models.DecimalField(max_digits=15, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    def __str__(self) -> str:
+        return self.name
