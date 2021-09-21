550e63938ee2fad631742ab4b521da5660ad7249
Tomasmed18 <tomy-medina@hotmail.com>
2021-09-20 23:05:26 -0300

    Create views to get products

--- mysite/shoppingcart/urls.py
+++ mysite/shoppingcart/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('product/all', views.get_all_products, name='all products'),
+    path('product/<int:product_id>', views.get_product, name='get product'),
 ]
--- mysite/shoppingcart/views.py
+++ mysite/shoppingcart/views.py
@@ -1,5 +1,15 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from .models import Product
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
     return HttpResponse('This is shoppingcart App')
+
+def get_all_products(request):
+    all_products = Product.objects.all()
+    output = ', '.join([p.name for p in all_products])
+    return HttpResponse(output)
+
+def get_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return HttpResponse(product)
